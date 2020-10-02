f6dbcd0cbacf4fa77b22ea0ab1435bdff88faf5c
Nikolay-71 <71880986+Nikolay-71@users.noreply.github.com>
2020-10-02 19:47:38 +0300

    Revert "lesson2"

--- task2.py
+++
@@ -1,5 +0,0 @@
-time = int(input("Введите время в секундах"))
-hour = time // 3600
-m = (time - hour * 3600) // 60
-sec = (time - hour * 3600 - m * 60)
-print("Ваше время {} ч {} мин {} сек".format(hour, m, sec))
--- task3.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Введите число"))
-b = a + a * 10
-c = a + b * 10
-n = a + b + c
-print("Ваше число: {}".format(n))
