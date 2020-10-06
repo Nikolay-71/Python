f1bbd67308e699a939bdeb6aca563b6c0d3ed527
Nikolay-71 <71880986+Nikolay-71@users.noreply.github.com>
2020-10-06 18:03:46 +0300

    Update task 3_6.py

--- task 3_6.py
+++ task 3_6.py
@@ -1,11 +1,1 @@
-while True:
-    a = input("Введите число или s для остановки программы: ")
-    b = a.split(" ")
-    sum_1 = 0
-    for word in b:
-        if word == 's':
-            break
-            print(f"Сумма {sum_1}. Программа остановлена")
-            value = float(word)
-            sum_1 += value
-        print(sum_1)
+
